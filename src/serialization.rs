e0ffc6a5ac73db7dcccae579adf73dcf6a1de77f
heinwol <heinvvol@gmail.com>
2024-12-18 23:56:55 +0300

    feat: consistent serailizing without boilerplate done

--- src/serialization.rs
+++ src/serialization.rs
@@ -1,8 +1,9 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::LazyLock};
 
 use crate::{tags::ParsedAO3Tags, utils::vec_as_newlines};
 use anyhow::bail;
-use rust_xlsxwriter::{Format, Worksheet};
+use log::warn;
+use rust_xlsxwriter::{Color, Format, Worksheet};
 use serde::Serialize;
 use serde_aux::prelude::serde_introspect;
 
@@ -20,64 +21,30 @@ pub struct FullFicInfo {
     pub tags: ParsedAO3Tags,
 }
 
-// pub fn to_flatten_maptree<T>(key_separator: &str, prefix: Option<&str>, src: &T) -> Result<Vec<serde_json::Value>, serde_json::error::Error>
-//     where T: serde::Serialize + ?Sized {
-//     Ok(serde::ser::SerializeStruct::new(key_separator.into(), prefix.unwrap_or("").into())
-//         .disassemble("", "", &serde_json::to_value(src)?))
-// }
-
-// #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-// pub struct FullFicInfoUnwrapped {
-//     pub path_to_file: PathBuf,
-//     pub title: Option<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub creators: Vec<String>,
-
-//     pub rating: Option<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub archive_warnings: Vec<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub categories: Vec<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub fandoms: Vec<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub relationships: Vec<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub characters: Vec<String>,
-//     #[serde(serialize_with = "vec_as_newlines")]
-//     pub additional_tags: Vec<String>,
-//     pub language: Option<String>,
-//     pub series: Option<String>,
-//     pub stats: Option<String>,
-// }
-
-// impl From<FullFicInfo> for FullFicInfoUnwrapped {
-//     fn from(value: FullFicInfo) -> Self {
-//         FullFicInfoUnwrapped {
-//             path_to_file: value.path_to_file,
-//             title: value.title,
-//             creators: value.creators,
-
-//             rating: value.tags.rating,
-//             archive_warnings: value.tags.archive_warnings,
-//             categories: value.tags.categories,
-//             fandoms: value.tags.fandoms,
-//             relationships: value.tags.relationships,
-//             characters: value.tags.characters,
-//             additional_tags: value.tags.additional_tags,
-//             language: value.tags.language,
-//             series: value.tags.series,
-//             stats: value.tags.stats,
-//         }
-//     }
-// }
+static FICMETAINFO_FIELD_NAMES: LazyLock<&[&str]> =
+    LazyLock::new(|| serde_introspect::<FicMetaInfo>());
+static PARSEDAO3TAGS_FIELD_NAMES: LazyLock<&[&str]> =
+    LazyLock::new(|| serde_introspect::<ParsedAO3Tags>());
 
 fn serialize_struct_fields_to_vec_of_string<S: Serialize>(
     strct: &S,
+    field_names: &'static [&str],
 ) -> anyhow::Result<Vec<String>> {
     let ser = serde_json::value::to_value(strct)?;
     match ser {
-        serde_json::Value::Object(map) => Ok(map.values().map(|elt| elt.to_string()).collect()),
+        serde_json::Value::Object(map) => Ok(field_names
+            .iter()
+            .map(|&field_name| {
+                let field_value = map[field_name].to_owned();
+                let parsed_value = serde_json::from_value::<Option<String>>(field_value.clone());
+                parsed_value
+                    .unwrap_or_else(|err| {
+                        warn!("cannot convert serialized data to string:\n\t{}", err);
+                        Some("".into())
+                    })
+                    .unwrap_or("".into())
+            })
+            .collect()),
         _ => bail!("bad serde struct: {:?}", ser),
     }
 }
@@ -96,12 +63,26 @@ pub fn write_fic_to_worksheet_row(
     worksheet: &mut Worksheet,
     row: usize,
     fic_info: &FullFicInfo,
-) -> anyhow::Result<()> {
-    let res = [
-        serialize_struct_fields_to_vec_of_string(&fic_info.meta_info)?,
-        serialize_struct_fields_to_vec_of_string(&fic_info.tags)?,
-    ]
-    .concat();
-    worksheet.write_row(row.try_into().unwrap_or(u32::MAX), 0, res)?;
-    Ok(())
+) -> () {
+    let row = row.try_into().unwrap_or(u32::MAX);
+    let mut perform_operation = || -> anyhow::Result<()> {
+        let res = [
+            serialize_struct_fields_to_vec_of_string(
+                &fic_info.meta_info,
+                &FICMETAINFO_FIELD_NAMES,
+            )?,
+            serialize_struct_fields_to_vec_of_string(&fic_info.tags, &PARSEDAO3TAGS_FIELD_NAMES)?,
+        ]
+        .concat();
+        worksheet.write_row(row, 0, res)?;
+        Ok(())
+    };
+    perform_operation().inspect_err(|err| {
+        worksheet.write_string_with_format(
+            row,
+            0,
+            err.backtrace().to_string(),
+            &Format::new().set_font_color(Color::Red),
+        );
+    });
 }
